f5eb037ce8451760e3895498ff8700bd205f85b9
Wesley Mauk <ShadowWesley77@hotmail.com>
2024-10-18 21:09:27 -0400

    Swap to WSGI, add short address to display
    Swapped to WSGI so that pythonanywhere would work.
    Added short address to display

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import json
+from wsgiref.simple_server import make_server
+
 import falcon
-import falcon.asgi
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
 import requests as requests
 
-
 class BalanceResource:
-    @staticmethod
-    async def on_get(req, resp):
+    def on_get(self, req, resp):
         # Get address from the request
         address = req.get_param('address') or ''
+        short_address = address[:12] + '...' + address[-6:]
 
         if '' == address:
             raise falcon.HTTPBadRequest(
@@ -29,9 +29,10 @@ class BalanceResource:
         # execute balance request
         try:
             x = requests.post(url, json=post_body)
-        except Exception:
+        except Exception as e:
             raise falcon.HTTPBadRequest(
-                title="Failed to retrieve balance"
+                title="Failed to retrieve balance",
+                description=str(e)
             )
 
         # Get balance from balance request
@@ -67,7 +68,7 @@ class BalanceResource:
 
         # Build response body
         body = {
-            'address': address,
+            'address': short_address,
             'balance': f'{balance:,}',
             'pricePerCoin': price,
             'USDBalance': f'{usd_balance:,}'
@@ -77,12 +78,20 @@ class BalanceResource:
         resp.text = json.dumps(body)
 
 
-# falcon.asgi.App instances are callable ASGI apps...
+# falcon.App instances are callable WSGI apps
 # in larger applications the app is created in a separate file
-app = falcon.asgi.App()
+application = falcon.App()
 
 # Resources are represented by long-lived class instances
 balance_resource = BalanceResource()
 
 # things will handle all requests to the '/things' URL path
-app.add_route('/balance', balance_resource)
+application.add_route('/balance', balance_resource)
+
+# For local testing
+if __name__ == '__main__':
+    with make_server('', 8000, application) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
